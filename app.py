c3c8a66521afce4a31f470a18a3b20defde7e6ce
chandu1717 <gchandu103@gmail.com>
2025-11-26 18:39:31 +0530

    first commit

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def welcome():
     return render_template('welcome.html')
 
 if __name__ == '__main__':
-    # ✅ MUST LISTEN ON ALL INTERFACES FOR PUBLIC ACCESS
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # ❌ no debug=True here – avoids trusted host security error
+    app.run(host="0.0.0.0", port=5000)
